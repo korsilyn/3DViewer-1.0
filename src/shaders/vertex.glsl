c5019ac58248c31badd229a7f12c134ded44671a
Sabra Harriette <sabrahar@oa-a5.msk.21-school.ru>
2023-08-18 15:39:11 +0300

    still not working

--- src/shaders/vertex.glsl
+++ src/shaders/vertex.glsl
@@ -1,11 +1,12 @@
 #version 330 core
 
 
-layout(location = 0) in vec3 position;
+layout(location = 0) in vec4 position;
 
 uniform mat4 modelMatrix;
 uniform mat4 projectionMatrix;
 
 void main() {
-  gl_Position = projectionMatrix * modelMatrix * vec4(position, 1.0);
+//  gl_Position = projectionMatrix * modelMatrix * vec4(position, 1.0);
+    gl_Position = projectionMatrix * modelMatrix * position;
 }
