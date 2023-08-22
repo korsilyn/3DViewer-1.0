73542b6161736f69c1d0ab0ac53329ab553fc642
Eugene (korsilyn) Gladkov <korsilyn@pm.me>
2023-08-22 15:17:24 +0300

    Test rewritten

--- src/tests/s21_test.c
+++ src/tests/s21_test.c
@@ -1,20 +1,24 @@
-//#include "s21_3dviewer.h"
-#include "s21_parse.h"
+// #include "s21_3dviewer.h"
 #include <stdlib.h>
 
-int main(void) {
-	obj_data data = {0};
-	//char file[] = "../cube.obj";
-	int status = s21_read_obj_file(&data, "/home/korsilyn/sber21/C8_3DViewer_v1.0-2/src/cube.obj");
-	if (status == 0) { printf("Amount of vertex: %ld\nArray: ", data.vertex_count);
-	for (size_t i = 0; i < data.vertex_count * 3; i++) printf("%lf ", data.vertex_array[i]);
-	printf("\n");
-	printf("Amount of vertex indices: %ld\nArray: ", data.vertex_indices_count);
-	for (size_t i = 0; i < data.vertex_indices_count * 2; i++) printf("%d ", data.vertex_indices_array[i]);
-	printf("\n");
-	}
-	else printf("%d", status);
-	s21_dealloc_data(&data);
-	return 0;
-}
+#include "s21_parse.h"
 
+int main(void) {
+  obj_data data = {0, NULL, 0, NULL};
+  // char file[] = "../cube.obj";
+  int status = s21_read_obj_file(
+      &data, "/home/korsilyn/sber21/C8_3DViewer_v1.0-2/src/cube.obj");
+  if (status == 1) {
+    printf("Amount of vertex: %ld\nArray: ", data.vertex_count);
+    for (size_t i = 0; i < data.vertex_count * 3; i++)
+      printf("%lf ", data.vertex_array[i]);
+    printf("\n");
+    printf("Amount of vertex indices: %ld\nArray: ", data.vertex_indices_count);
+    for (size_t i = 0; i < data.vertex_indices_count * 2; i++)
+      printf("%d ", data.vertex_indices_array[i]);
+    printf("\n");
+  } else
+    printf("%d", status);
+  s21_dealloc_data(&data);
+  return 0;
+}
