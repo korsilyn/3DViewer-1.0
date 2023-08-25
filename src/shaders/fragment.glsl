5a1439ac918abf64c09fc0b5121b00ef5045a68e
Eugene (korsilyn) Gladkov <korsilyn@pm.me>
2023-08-25 09:23:26 +0300

    Update shaders

--- src/shaders/fragment.glsl
+++ src/shaders/fragment.glsl
@@ -1,5 +1,4 @@
 #version 330 core
-out vec4 fragColor;
 uniform vec4 edgeColor;
 
-void main() { fragColor = edgeColor; }
+void main() { gl_FragColor = edgeColor; }
--- src/shaders/vertex.glsl
+++ src/shaders/vertex.glsl
@@ -2,9 +2,8 @@
 layout(location = 0) in vec4 position;
 
 uniform mat4 modelMatrix;
-uniform mat4 viewMatrix;
 uniform mat4 projectionMatrix;
 
 void main() {
-  gl_Position = projectionMatrix * viewMatrix * modelMatrix * position;
+  gl_Position = projectionMatrix * modelMatrix * position;
 }
